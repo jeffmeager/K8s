c3427640a3904fde37aa695316a93663fea56795
Jeff Meager <jeff@meager.net>
2025-06-02 17:33:36 +0800

    Changes to be committed: 	modified:   Jenkinsfile.state 	modified:   Jenkinsfile.teardown 	modified:   terraform-state/main.tf 	modified:   terraform/variables.tf

--- terraform-state/main.tf
+++ terraform-state/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "jeffmeager-challenge-terraform-state-bucket"
+  bucket = var.state_bucket
 
   lifecycle {
     prevent_destroy = true
