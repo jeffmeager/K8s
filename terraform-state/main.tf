8428419d9cf0680e62cd773a07ad94f85ceeed73
Jeff Meager <jeff@meager.net>
2025-06-02 14:47:40 +0800

    more errors in state create process. Moved test to Jenkinsfile.state

--- terraform-state/main.tf
+++ terraform-state/main.tf
@@ -3,14 +3,7 @@ provider "aws" {
   profile = "devops-admin"
 }
 
-# Try to retrieve the bucket if it already exists
-data "aws_s3_bucket" "existing" {
-  bucket = "jeffmeager-challenge-terraform-state-bucket"
-}
-
-# Only create the bucket if it doesn't already exist
 resource "aws_s3_bucket" "terraform_state" {
-  count  = can(data.aws_s3_bucket.existing.id) ? 0 : 1
   bucket = "jeffmeager-challenge-terraform-state-bucket"
 
   lifecycle {
@@ -18,20 +11,16 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
-# Only configure versioning if the bucket is being created
 resource "aws_s3_bucket_versioning" "versioning" {
-  count  = length(aws_s3_bucket.terraform_state)
-  bucket = aws_s3_bucket.terraform_state[0].id
+  bucket = aws_s3_bucket.terraform_state.id
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-# Only apply encryption if the bucket is being created
 resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  count  = length(aws_s3_bucket.terraform_state)
-  bucket = aws_s3_bucket.terraform_state[0].bucket
+  bucket = aws_s3_bucket.terraform_state.bucket
 
   rule {
     apply_server_side_encryption_by_default {
