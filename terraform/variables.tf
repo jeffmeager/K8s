78b4019bf438fe9a89c60037128b5fd6950c03fa
Jeff Meager <jeff@meager.net>
2025-06-02 16:15:55 +0800

    updated Jenkinsfile.deploy and terraform/variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,3 +14,14 @@ variable "kubeconfig_path" {
   type    = string
   default = "~/.kube/config"
 }
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
