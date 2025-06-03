6f926be97bc724f61bd41aa11bee2e6521d40cf6
Jeff Meager <jeff@meager.net>
2025-06-03 21:11:49 +0800

    Changes to aws_auth to avoid issues with it pre-existing

--- terraform/aws_auth.tf
+++ terraform/aws_auth.tf
@@ -1,38 +1,31 @@
-data "aws_eks_cluster_auth" "eks" {
-  name = aws_eks_cluster.eks.name
-}
+resource "kubernetes_manifest" "aws_auth" {
+  manifest = {
+    apiVersion = "v1"
+    kind       = "ConfigMap"
+    metadata = {
+      name      = "aws-auth"
+      namespace = "kube-system"
+    }
+    data = {
+      mapRoles = yamlencode([
+        {
+          rolearn  = aws_iam_role.eks_node.arn
+          username = "system:node:{{EC2PrivateDNSName}}"
+          groups   = [
+            "system:bootstrappers",
+            "system:nodes"
+          ]
+        },
+        {
+          rolearn  = "arn:aws:iam::559050241687:role/AWSReservedSSO_AdministratorAccess_69ccaa80db7e44d3"
+          username = "admin"
+          groups   = [
+            "system:masters"
+          ]
+        }
+      ])
+    }
+  }
 
-provider "kubernetes" {
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
-
-resource "kubernetes_config_map" "aws_auth" {
   depends_on = [aws_eks_node_group.node]
-
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
-
-  data = {
-    mapRoles = yamlencode([
-      {
-        rolearn  = aws_iam_role.eks_node.arn
-        username = "system:node:{{EC2PrivateDNSName}}"
-        groups   = [
-          "system:bootstrappers",
-          "system:nodes"
-        ]
-      },
-      {
-        rolearn  = "arn:aws:iam::559050241687:role/AWSReservedSSO_AdministratorAccess_69ccaa80db7e44d3"
-        username = "admin"
-        groups   = [
-          "system:masters"
-        ]
-      }
-    ])
-  }
 }
