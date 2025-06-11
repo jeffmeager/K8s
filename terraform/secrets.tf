682c8b47b3f2dd3f1bfeb8406531cf90d299d668
Jeff Meager <jeff@meager.net>
2025-06-11 16:26:33 +1000

    Removed CSI driver. Moved to storing env vars for app into K8 via terraform

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,30 +1,10 @@
-# resource "aws_secretsmanager_secret" "mongodb_username" {
-#   name        = "mongodb_username"
-#   description = "MongoDB admin username"
-# }
-
-# resource "aws_secretsmanager_secret_version" "mongodb_username_version" {
-#   secret_id     = aws_secretsmanager_secret.mongodb_username.id
-#   secret_string = var.mongodb_username
-# }
-
-# resource "aws_secretsmanager_secret" "mongodb_password" {
-#   name        = "mongodb_password"
-#   description = "MongoDB admin password"
-# }
-
-# resource "aws_secretsmanager_secret_version" "mongodb_password_version" {
-#   secret_id     = aws_secretsmanager_secret.mongodb_password.id
-#   secret_string = var.mongodb_password
-# }
-
 resource "random_password" "secret_key" {
   length  = 32
   special = false
 }
 
 resource "aws_secretsmanager_secret" "webapp_secrets" {
-  name = "webapp-secrets"
+  name        = "webapp-secrets"
   description = "Secrets for WebApp (MONGODB_URI and SECRET_KEY)"
 }
 
@@ -38,5 +18,24 @@ resource "aws_secretsmanager_secret_version" "webapp_secrets_version" {
 
   depends_on = [
     aws_instance.mongodb_instance
+  ]
+}
+
+resource "kubernetes_secret" "webapp_secrets" {
+  metadata {
+    name      = "webapp-secrets"
+    namespace = "default"
+  }
+
+  data = {
+    mongodb-uri = base64encode("mongodb://${var.mongodb_username}:${var.mongodb_password}@${aws_instance.mongodb_instance.public_ip}:27017")
+    secret-key  = base64encode(random_password.secret_key.result)
+  }
+
+  type = "Opaque"
+
+  depends_on = [
+    aws_instance.mongodb_instance,
+    aws_secretsmanager_secret_version.webapp_secrets_version
   ]
 }
