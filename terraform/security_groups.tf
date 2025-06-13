8bcebf2e1ef16982ef78fd194f04d256a49f6dd6
Jeff Meager <jeff@meager.net>
2025-06-14 00:50:47 +0800

    typo in cidr block

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "mongodb_sg" {
     from_port       = 27017
     to_port         = 27017
     protocol        = "tcp"
-    security_groups = ["10.0.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
