9bbf5e9949b5dcbbc752daf0b6ba6af266918a80
Jeff Meager <jeff@meager.net>
2025-06-14 14:09:10 +0800

    missing S3 cloudtrail policy

--- terraform/aws_prev&detect.tf
+++ terraform/aws_prev&detect.tf
@@ -26,6 +26,42 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
   }
 }
 
+# Required for CloudTrail to write logs
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket_policy" "cloudtrail_logs" {
+  bucket = aws_s3_bucket.cloudtrail_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailAclCheck",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action   = "s3:GetBucketAcl",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.bucket}"
+      },
+      {
+        Sid       = "AWSCloudTrailWrite",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action   = "s3:PutObject",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.bucket}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_cloudtrail" "main" {
   name                          = "challenge-cloudtrail"
   s3_bucket_name                = aws_s3_bucket.cloudtrail_bucket.id
